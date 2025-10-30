4d1824c673f1b0acb351021e8e46e48bb5c8baa0
ThereptileII <swedishcountrysideliving@gmail.com>
2025-10-30 18:11:40 +0100

    Fix LVGL indev read period fallback

--- lvgl_indev_compat.h
+++ lvgl_indev_compat.h
@@ -8,16 +8,12 @@ static inline void lvgl_set_indev_read_period(lv_indev_t *indev, uint32_t period
     lv_indev_set_read_period(indev, period_ms);
   }
 #else
-  (void)indev;
-  lv_disp_t *disp = lv_disp_get_default();
-  if (!disp) {
-    disp = lv_disp_get_next(NULL);
+  if (!indev) {
+    return;
   }
-  if (disp) {
-    lv_timer_t *read_timer = lv_indev_get_read_timer(disp);
-    if (read_timer) {
-      lv_timer_set_period(read_timer, period_ms);
-    }
+  lv_timer_t *read_timer = lv_indev_get_read_timer(indev);
+  if (read_timer) {
+    lv_timer_set_period(read_timer, period_ms);
   }
 #endif
 }
